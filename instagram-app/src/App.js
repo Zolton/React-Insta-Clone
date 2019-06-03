f41baabfb9a9a19deb256f68b912e36a96d926a6
Zolton <gmzesq@gmail.com>
2019-06-03 15:24:27 -0400

    Used NPM to create the react app

---
+++ instagram-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import dummyData from './';
+
+function App() {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
