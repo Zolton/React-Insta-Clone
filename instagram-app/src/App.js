2087fe47e8035fab2070ad80baa0561e46c53c63
Zolton <gmzesq@gmail.com>
2019-06-05 16:02:35 -0400

    Moved App moving parts to PostsPage, still broken

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -9,50 +9,11 @@ import withAuthenticate from "./components/authentication/withAuthenticate"
 const ComponentFromWithAuthenticate = withAuthenticate(<PostsPage />)
 
 class App extends React.Component {
-  state = { 
-    data: [],
-    filteredPosts: []
-  };
- 
-//BOOM! No more constructor and super
-// Why? Because syntactic sugar in new react - constructor props + super props
-//is just assumed!
-
-  componentDidMount() {
-    this.setState({ data: dummyData });
-  }
-
   
-  searchfilter = e => {
-    const filtered = this.state.data.filter(post => post.username.toLowerCase().includes(e.target.value.toLowerCase()))
-//check if username is equal to value, toLowercase is just in case of aberations innames
-  this.setState({ filteredPosts: filtered})
-    //when a hit results, send it to the state above, filteredPosts
-  }
-  
-
- 
-  // const newFilter = this.state.data.filter(post => 
-    //   post.username === this.state.search
-    // )
-  //wanna filter our data ^^
-  //take this.state.data, and instead of mapping it, FILTER it
-  //go over each surface level object, assign it to post, and check if - 
-  //filter posts, only show usersnames equal to the search
-  //remember filter gets RID of things, opposite of map
-
   render() {
-    // maps thru dummyData, sends each object (3) to PostContainer as
-    // its own set of data
-    
     return (
       <div className="App">
-         <SearchBar 
-          newSearch={this.state.search}
-          searchFilter={this.searchFilter}
-        />
         <ComponentFromWithAuthenticate />
-        <PostContainer data={this.state.data} filteredPosts={this.state.filteredPosts} searchFilter={this.searchFilter} />
     </div>
     );
   }
--- instagram-app/src/components/PostContainer/PostsPage.js
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -1,11 +1,58 @@
 import React from "react"
+import "./App.css";
+import dummyData from "./dummy-data";
+import PostContainer from "./components/PostContainer/PostContainer";
+import SearchBar from "./components/SearchBar/SearchBar"
+import withAuthenticate from "./components/authentication/withAuthenticate"
+
 
 class PostsPage extends React.Component {
-    render () {
+
+          //BOOM! No more constructor and super
+// Why? Because syntactic sugar in new react - constructor props + super props
+//is just assumed!
+
+    state = { 
+        data: [],
+        filteredPosts: []
+      };
+
+      componentDidMount() {
+        this.setState({ data: dummyData });
+      }
+
+      searchfilter = e => {
+        const filtered = this.state.data.filter(post => post.username.toLowerCase().includes(e.target.value.toLowerCase()))
+    //check if username is equal to value, toLowercase is just in case of aberations innames
+      this.setState({ filteredPosts: filtered})
+        //when a hit results, send it to the state above, filteredPosts
+      }
+
+  // const newFilter = this.state.data.filter(post => 
+    //   post.username === this.state.search
+    // )
+  //wanna filter our data ^^
+  //take this.state.data, and instead of mapping it, FILTER it
+  //go over each surface level object, assign it to post, and check if - 
+  //filter posts, only show usersnames equal to the search
+  //remember filter gets RID of things, opposite of map
+
+      render() {
+        // maps thru dummyData, sends each object (3) to PostContainer as
+        // its own set of data
+        
         return (
-            <div>Posts Page rendering</div>
-        )
-    }
+          <div className="App">
+             <SearchBar 
+              newSearch={this.state.search}
+              searchFilter={this.searchFilter}
+            />
+            <PostContainer 
+            data={this.state.data} 
+            filteredPosts={this.state.filteredPosts} 
+            searchFilter={this.searchFilter} 
+            />
+        </div>
 }
 
 export default PostsPage
